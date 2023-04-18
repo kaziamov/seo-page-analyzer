3e5673e1a548a6f169ce73df465be1aaefd501ef
Ilia Kaziamov <kaziamov@outlook.com>
2023-04-18 03:52:14 +0300

    Refactor to pool sql connection

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 from page_analyzer.models import is_checks_exist, get_data_by_id, get_checks_by_id, get_all_urls, add_new_check, add_new_url, is_url_exist
 from page_analyzer.parsing import get_data
 from page_analyzer.validation import is_valid
+from page_analyzer.db_connect import conn_pool
+
 
 
 app = Flask(__name__)
@@ -14,6 +16,13 @@ app.permanent_session_lifetime = timedelta(hours=24)
 
 title = "Page Analyzer"
 
+class getcursor(object):
+    def __enter__(self):
+        con = conn_pool.getconn()
+        return con
+
+    def __exit__(self, *args):
+        conn_pool.putconn(con)
 
 @app.route('/')
 def home(url='', status=200):
@@ -25,49 +34,58 @@ def urls_id(id, status=200):
     name = ''
     date = ''
     checks = []
-    url_data = get_data_by_id(id)
+    conn = conn_pool.getconn()
+    url_data = get_data_by_id(conn, id)
     if url_data:
         name = url_data[1]
         date = url_data[2]
-    if is_checks_exist(id):
-        checks = get_checks_by_id(id)
+    if is_checks_exist(conn, id):
+        checks = get_checks_by_id(conn, id)
+    conn_pool.putconn(conn)
     return render_template("urls_id.html", title=name, name=name, date=date, id=id, checks=checks), status
 
 
 @app.route("/urls/<int:id>/checks", methods=['POST'])
 def url_check(id):
+    conn = conn_pool.getconn()
     if request.method == 'POST':
-        url = get_data_by_id(id)[1]
+        url = get_data_by_id(conn, id)[1]
         data = get_data(url)
         if data:
-            add_new_check(id, *data)
+            add_new_check(conn, (id, *data))
             flash('Страница успешно проверена', 'success')
-            return redirect(url_for('urls_id', id=id))
+            status = 200
         else:
             flash('Произошла ошибка при проверке', 'danger')
-            return redirect(url_for('urls_id', id=id, status=422))
+            status = 422
+        conn.commit()
+        conn_pool.putconn(conn)
+        return redirect(url_for('urls_id', id=id, status=status))
 
 
 @app.route("/urls", methods=["GET", "POST"])
 def urls():
+    conn = conn_pool.getconn()
     if request.method == 'POST':
         url = request.form.get("url").strip()
         if is_valid(url):
-            id = is_url_exist(url)
+            id = is_url_exist(conn, url)
             if id is not False:
                 flash("Страница уже существует", 'primary')
             else:
-                add_new_url(url)
+                add_new_url(conn, url)
                 flash("Страница успешно добавлена", 'success')
-                id = is_url_exist(url)
+                id = is_url_exist(conn, url)
+            conn.commit()
+            conn_pool.putconn(conn)
             return redirect(url_for('urls_id', id=id))
 
-        else:
-            flash("Некорректный URL", 'danger')
-            return render_template("home.html", title=title, url=url), 422
+        flash("Некорректный URL", 'danger')
+        return render_template("home.html", title=title, url=url), 422
 
     else:
-        urls = get_all_urls()
+        urls = get_all_urls(conn)
+        conn_pool.putconn(conn)
         return render_template("urls.html", title=title, urls=urls)
 
 
---
+++ page_analyzer/db_connect.py
@@ -0,0 +1,27 @@
+import psycopg2
+from psycopg2 import pool
+from page_analyzer.settings import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
+
+
+def create_connection(*args, **kwargs):
+    """Create connection for work with PostgresSQL"""
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME)
+
+
+def create_pool(min_conn=1, max_conn=5):
+    """Create connection for work with PostgresSQL"""
+    return pool.SimpleConnectionPool(minconn=min_conn,
+                                     maxconn=max_conn,
+                                     connection_factory=create_connection,
+                                     host=DB_HOST,
+                                     port=DB_PORT,
+                                     user=DB_USER,
+                                     password=DB_PASS,
+                                     database=DB_NAME)
+
+conn_pool = create_pool()
